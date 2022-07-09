3f9023335d488063ae781bcd76e434140c90d1e5
Volkan Aydıngül <volkanaydingul@gmail.com>
2022-07-10 01:46:30 +0300

    Fixes on dataset and some dummy data files to check code

--- expert_dataset.py
+++ expert_dataset.py
@@ -1,9 +1,11 @@
 from enum import Enum
-from torch.utils.data import Dataset
+from git import Object
+from torch.utils.data import Dataset, DataLoader
 import os
 from PIL import Image
 import numpy as np
 import json
+import torch
 
 
 
@@ -33,31 +35,33 @@ class ExpertDataset(Dataset):
         measurements_actions = sorted(os.listdir(os.path.join(self.data_root, "measurements")))
         self._length = len(rgb_files)
 
-        self._images = []
-        self._steer = []
-        self._throttle = []
-        self._break = []
-        self._speed = []
-        self._command = []
-        self._route_dist = []
-        self._route_angle = []
-        self._lane_dist = []
-        self._lane_angle = []
-        self._hazard = []
-        self._hazard_dist = []
-        self._tl_state = []
-        self._tl_dist = []
-        self._is_junction = []
+        self._images = torch.zeros((self._length, 512, 512, 4), dtype = torch.float32)
+        self._steer = torch.zeros((self._length, ), dtype = torch.float32)
+        self._throttle = torch.zeros((self._length, ), dtype = torch.float32)
+        self._brake = torch.zeros((self._length, ), dtype = torch.float32)
+        self._speed = torch.zeros((self._length, ), dtype = torch.float32)
+        self._command = torch.zeros((self._length, ), dtype = torch.float32)
+        self._route_dist = torch.zeros((self._length, ), dtype = torch.float32)
+        self._route_angle = torch.zeros((self._length, ), dtype = torch.float32)
+        self._lane_dist = torch.zeros((self._length, ), dtype = torch.float32)
+        self._lane_angle = torch.zeros((self._length, ), dtype = torch.float32)
+        self._hazard = torch.zeros((self._length, ), dtype = torch.float32)
+        self._hazard_dist = torch.zeros((self._length, ), dtype = torch.float32)
+        self._tl_state = torch.zeros((self._length, ), dtype = torch.float32)
+        self._tl_dist = torch.zeros((self._length, ), dtype = torch.float32)
+        self._is_junction = torch.zeros((self._length, ), dtype = torch.float32)
         
-        for k in range(len(self.rgb_files)):
+        for k in range(len(rgb_files)):
             # Read images as a NumPy array
-            self._images[k] = np.asarray(Image.open(os.path.join(self.data_root, "rgb", rgb_files[k])))
+
+            
+            self._images[k] = torch.from_numpy(np.array(Image.open(os.path.join(self.data_root, "rgb", rgb_files[k]))))
             # Read steer angle command from json file
             with open(os.path.join(self.data_root, "measurements", measurements_actions[k]), "r") as f:
                 json_content = json.load(f)
                 self._steer[k] = json_content["steer"]
                 self._throttle[k] = json_content["throttle"]
-                self._break[k] = json_content["break"]
+                self._brake[k] = json_content["brake"]
                 self._speed[k] = json_content["speed"]
                 self._command[k] = json_content["command"]
                 self._route_dist[k] = json_content["route_dist"]
@@ -70,22 +74,22 @@ class ExpertDataset(Dataset):
                 self._tl_dist[k] = json_content["tl_dist"]
                 self._is_junction[k] = json_content["is_junction"]
 
-                
+     
 
         
 
     def __getitem__(self, index):
         """Return RGB images and measurements"""
     
-        if self.learning_type == 0:
+        if self.learning_type == LearningType.IMITATION:
             
-            return self._images[index], self._command[index], self._speed[index], self._steer[index], self._throttle[index], self._break[index] 
+            return self._images[index], self._command[index], self._speed[index], self._steer[index], self._throttle[index], self._brake[index] 
 
-        elif self.learning_type == 1:
+        elif self.learning_type == LearningType.AFFORDANCE:
 
             return self._images[index], self._command[index], self._lane_dist[index], self._lane_angle[index], self._tl_dist[index], self._tl_state[index]
              
-        elif self.learning_type == 2:
+        elif self.learning_type == LearningType.REINFORCEMENT:
             
             NotImplementedError("Data acquisition for reinforcement learning is not implemented yet")
 
@@ -95,4 +99,22 @@ class ExpertDataset(Dataset):
 
     def __len__(self):
 
-        return len(self.length)
+        return self._length
+
+if __name__=="__main__":
+
+    expert_dataset = ExpertDataset("./dataset/", learning_type = LearningType.IMITATION)
+
+
+    train_loader = DataLoader(expert_dataset, batch_size=1, shuffle=True,
+                              drop_last=True) 
+    
+    print(expert_dataset[0])
+    for i, (images, command, speed, steer, throttle, brake) in enumerate(train_loader):
+        print(images.shape)
+        print(command.shape)
+        print(speed.shape)
+        print(steer.shape)
+        print(throttle.shape)
+        print(brake.shape)
+        break
